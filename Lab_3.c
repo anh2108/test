9c7da532b251c61663cb36ae0235793a6238bff2
anh2108 <118011163+anh2108@users.noreply.github.com>
2022-11-13 00:58:26 +0700

    addf

--- Lab_3.c
+++ Lab_3.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include<math.h>
 
 int main() {
 
-	//bai 1
-	/*printf("Bai 1:");
+	// bai 1
+	printf("Bai 1:");
 	double diem;
-	Nhaplai ://lenh goto, diem khoi chay lai
+Nhaplai://lenh goto, diem khoi chay lai
 	printf("\nNhap vao diem = ");
 	scanf_s("%lf", &diem);
 
@@ -13,25 +14,93 @@ int main() {
 		printf("\nDiem khong hop le, so diem phai tu 0 den 10");
 		goto Nhaplai;//lenh goto, sau khi cau lenh if sai khoi chay lai 
 	}
-	
+
 	if (diem >= 9) {
-		printf("\nHoc luc xuat sac");
+		printf("\nHoc luc xuat sac\n");
 	}
 	else if (diem >= 8) {
-		printf("\nHoc luc gioi");
+		printf("\nHoc luc gioi\n");
 	}
 	else if (diem >= 6.5) {
-		printf("\nHoc luc kha");
+		printf("\nHoc luc kha\n");
 	}
 	else if (diem >= 5) {
-		printf("\nHoc luc trung binh");
+		printf("\nHoc luc trung binh\n");
 	}
 	else if (diem >= 3.5) {
-		printf("\nHoc luc yeu");
+		printf("\nHoc luc yeu\n");
 	}
 	else if (diem < 3.5) {
-		printf("\nHoc luc kem");
-	}*/
+		printf("\nHoc luc kem\n");
+	}
+
+	//Bai 2
+	printf("\nBai 2.1:");
+	double a, b, x;
+	printf("\nNhap vao cac gia tri cua phuong trinh: ");
+	printf("\na = ");
+	scanf_s("%lf", &a);
+	printf("\nb = ");
+	scanf_s("%lf", &b);
+	x = -b / a;
+
+	if (a == 0) {
+		if (b == 0) {
+			printf("\nPhuong trinh co vo so nghiem");
+		}
+		else {
+			printf("\nPhuong trinh vo nghiem");
+		}
+	}
+	else {
+		printf("\nPhuong trinh co nghiem x = %f", x);
+	}
+
+
+
+	printf("\nBai 2.2 :");
+	double d, e, f, x0, denta, x1, x2;
+	printf("Nhap vao cac he so cua phuong trinh bac 2: ");
+	printf("\nd= ");
+	scanf_s("%lf", &d);
+	printf("\ne = ");
+	scanf_s("%lf", &e);
+	printf("\nf = ");
+	scanf_s("%lf", &f);
+
+	if (d == 0) {
+		printf("\nPhuong trinh tro thanh ex - f = 0");
+		if (e == 0) {
+			if (f == 0) {
+				printf("\nPhuong trinh co vo so nghiem");
+			}
+			else {
+				printf("\nPhuong trinh vo nghiem");
+			}
+		}
+		else {
+			x0 = -f / e;
+			printf("\nPhuong trinh co nghiem x = %.2lf", x0);
+		}
+
+	}
+	else {
+		denta = pow(e, 2) - 4 * d * f;
+		if (denta < 0) {
+			printf("\n Phuong trinh vo nghiem");
+		}
+		else if (denta == 0) {
+			x0 = -e / (2 * d);
+			printf("\nPhuong trinh co nghiem kep x = %.2lf", x0);
+		}
+		else {
+			x1 = (-e - sqrt(denta)) / (2 * d);
+			x2 = (-e + sqrt(denta)) / (2 * d);
+			printf("\nPhuong trinh co hai nghiem phan biet x1 = %.2lf \n x2 = %.2lf", x1, x2);
+		}
+	}
+
+
 
 	//bai 3
 	printf("\nBai 3:");
@@ -72,6 +141,38 @@ int main() {
 		printf("\nSo tien phai dong o bac 6 la: %.3lf VND", soTiendong);
 	}
 
-	return 0;
+	//bai 4
+	printf("\nBai 4: ");
+	printf("\n******       BAI TAP      ******");
+	printf("\n<*****1. Xep loai hoc sinh*****>");
+	printf("\n<****2.Giai bat phong trinh****>");
+	printf("\n<******3. Tinh tien dien*******>");
+	printf("\n<*4. MeNU chuong trinh bai tap*>");
+
+	int baiTapcanlam;
+	printf("\nBai tap can lam la : ");
+	scanf_s("%d", &baiTapcanlam);
+
+	switch (baiTapcanlam)
+	{
+	case 1 :
+		printf("\nBai 1: Xep loai hoc sinh");
+		break;
+	case 2 :
+		printf("\nBai 2: Giai bat phuong trinh");
+		break;
+	case 3 :
+		printf("\nBai 3: Tinh tien dien");
+		break;
+	case 4 :
+		printf("\nBai 4 : Menu chuong trinh bai tap");
+
+	default :
+		printf("\nDa het bai tap");
+		break;
+	}
+
+		return 0;
 
 }
+
